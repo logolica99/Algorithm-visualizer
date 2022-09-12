50896822d0ab48e1aa7c705a46b61b5d46bee877
Jubaer Jami <jamijubaer@gmail.com>
2022-09-12 10:34:51 +0600

    added mui slider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 import QuickSort from "./Components/QuickSort";
+import Slider from "@mui/material/Slider";
+
 
 function App() {
   const [length, setLength] = useState(4);
-  const minLength = 4;
+
   return (
     <div className="App">
-      <input
-        type="range"
-        min="4"
-        max="50"
-        onChange={(e) => {
-          setLength(e.target.value);
-        }}
-      />
+      <div className="inputRange">
+        <Slider
+          value={length}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+          min={5}
+          max={50}
+          onChange={(e) => {
+            setLength(e.target.value);
+          }}
+        />
+      </div>
+
       <QuickSort length={length} />
     </div>
   );
--- src/Components/QuickSort.js
+++ src/Components/QuickSort.js
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Bar from "./Bar";
 
 export default function QuickSort({ length }) {
-  const [numbers, setNumbers] = useState(
-    Array.from({ length: length }, () => Math.floor(Math.random() * 30))
-  );
-
-  useEffect(() => {
-    Array.from({ length: length }, () => Math.floor(Math.random() * 30));
-  }, [length]);
-
   return (
     <div>
-     
-
       <div className="allBars">
-        {Array.from({ length: length }, () => Math.floor(Math.random() * 30+1)).map((value) => (
+        {Array.from({ length: length }, () =>
+          Math.floor(Math.random() * 30 + 1)
+        ).map((value) => (
           <Bar times={value} key={Math.random()} />
         ))}
       </div>
